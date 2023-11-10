3143af2c852d782c00e4a6a4ae240746f65f7bf5
lai hung wei <128201642+s990093@users.noreply.github.com>
2023-11-10 20:48:31 +0800

    fix: seting.py

--- server/server/settings.py
+++ server/server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i$3i#v$0!$g(ry(@v7b2v-bydv=h=!8#i+f%y#%#6+neyfu2te
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -116,6 +116,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    # BASE_DIR / "myapp/static/css",
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
