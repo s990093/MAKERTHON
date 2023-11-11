3e82955c437845f98ccc9aa8b098f2ffe9822624
lai hung wei <128201642+s990093@users.noreply.github.com>
2023-11-12 01:18:24 +0800

    feat

--- App/App/Conf.swift
+++ App/App/Conf.swift
@@ -9,8 +9,8 @@ import Foundation
 
 
 struct AppConfig {
-//    static let serverIP = "http://49.213.238.75"
-    static let serverIP = "http://127.0.0.1"
+    static let serverIP = "http://49.213.238.75"
+//    static let serverIP = "http://127.0.0.1"
 
     static let serverPort = 8000
     static let baseUrl = "\(serverIP):\(serverPort)"
